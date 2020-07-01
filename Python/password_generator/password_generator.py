4b409827b3f88a66336caf9199b20cd3068e4f18
Meriicistyle <askinmeric79@gmail.com>
2020-07-01 18:08:20 +0300

    Update password_generator.py
    Stole code from the internet and changed it how I like. I hate this kind of behaviour so the link as a reference: https://www.practicepython.org/solution/2014/06/06/16-password-generator-solutions.html

--- Python/password_generator/password_generator.py
+++ Python/password_generator/password_generator.py
@@ -1,22 +1,7 @@
 import string
-import math
-import secrets
+import random
 
-while True:
-    print("""
-    Welcome to the password generator!
-    Built by Meriicistyle this tool
-    uses very 'basic' creating algo-
-    rithms. Used pure Python random.""")
+def password_generator(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+	return ''.join(random.choice(chars) for _ in range(size))
 
-#Variables sequentially for the password length, uppercase letters, number choice, special characters and redotime.
-    length = int(input("How long will your password be: "))
-    uppercase = str.lower(input("Upper case letters? (Y)es or (N)o: "))
-    lowercase = str.lower(input("Lower case letters? (Y)es or (N)o: "))
-    numchoice = str.lower(input("Should numbers be included? (Y)es or (N)o: "))
-    special = str.lower(input("Any special characters (e.g. %&+^^)? (Y)es or (N)o: "))
-    redotime = int(secrets.randbelow(length))
-
-    upperletters = string.ascii_lowercase
-    lowletters = string.ascii_uppercase
-    allletters = string.ascii_letters
+print(password_generator(int(input('How long will your password be?: '))))
