d85d3b56203f73d5620cd43c77a494a7154ac752
Meriicistyle <askinmeric79@gmail.com>
2020-07-01 18:57:36 +0300

    Update password_generator.py
    Added the missing spaces and description.

--- Python/password_generator/password_generator.py
+++ Python/password_generator/password_generator.py
@@ -1,7 +1,16 @@
 import string
 import random
 
-def password_generator(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
-	return ''.join(random.choice(chars) for _ in range(size))
+while True:
+    print("""        
+        --> Welcome to the Password
+            generator. Just give
+            your password length and
+            check the output. Parameters
+            will be added in the future.
+        """)
 
-print(password_generator(int(input('How long will your password be?: '))))
+    def password_generator(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
+	    return ''.join(random.choice(chars) for _ in range(size))
+
+    print(password_generator(int(input('How long will your password be?: '))))
