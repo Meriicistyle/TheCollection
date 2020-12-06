114af0021bbf0601412f91e1531c02ca27f943c8
Meriicistyle <askinmeric79@gmail.com>
2020-12-06 19:25:21 +0100

    Not good looking
    I'm stoopid

---
+++ Java/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        long duplicateinput = StdIn.readLong();
+
+        System.out.println(duplicateinput);
+    }
+
+}
