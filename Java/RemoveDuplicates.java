50216ba6929f6c2bac6ee62c99130243b2187c22
Meriicistyle <askinmeric79@gmail.com>
2020-12-20 23:03:25 +0100

    Update RemoveDuplicates.java

--- Java/RemoveDuplicates.java
+++ Java/RemoveDuplicates.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class RemoveDuplicates {
 
     public static void main(String[] args) {
-        long duplicateinput = StdIn.readLong();
-
-        System.out.println(duplicateinput);
+        String words[] = new 
     }
 
 }
